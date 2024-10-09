7edb459da25ac17dcbdc02d3648466bcfd387c2f
KoyomiHidaka <48902027+KoyomiHidaka@users.noreply.github.com>
2024-10-09 15:35:03 +0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,8 +1,5 @@
 import random
-
-
 from aiogram.types import Message
-
 import json
 import torch
 from model import NeuralNet
@@ -12,22 +9,14 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
-
-
 FILE = "data.pth"
-
 data = torch.load(FILE, weights_only=True)
-
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
 all_words = data["all_words"]
 tags = data["tags"]
 model_state = data["model_state"]
-
-
-
-
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
 model.load_state_dict(model_state)
